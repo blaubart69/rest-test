6db0e47058cf731f61133b53af45b58b1f0e469a
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2024-12-09 16:52:22 +0100

    added some CLI scripts for JBoss Elytron config

---
+++ exec_cli.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+jboss-eap-7.4/bin/jboss-cli.sh --connect --file=./rest-test/remove_chris.war.cli
--- src/main/java/at/spindi/TestRestController.java
+++ src/main/java/at/spindi/TestRestController.java
@@ -6,11 +6,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import jakarta.inject.Singleton;
+//import jakarta.inject.Singleton;
 
 @Path("/admins")
-@RolesAllowed("Users")
-@Singleton
+@RolesAllowed({"Bumsti"})
+//@Singleton
 public class TestRestController {
 
 	@GET
