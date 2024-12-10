a04e6e2b1fee0a4e1def4e121869096e53df95ba
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2024-12-10 18:01:33 +0100

    changed webxml
    - added context-param
    - added servlet-mapping

---
+++ 20_add_rest-test.war.sh
@@ -0,0 +1,1 @@
+../jboss-eap-7.4/bin/jboss-cli.sh --connect --file=./20_add_rest-test.war.cli
--- src/main/java/at/spindi/TestRestController.java
+++ src/main/java/at/spindi/TestRestController.java
@@ -4,19 +4,15 @@ import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
-
-//import jakarta.inject.Singleton;
 
 @Path("/admins")
-@RolesAllowed({"Users"})
-//@Singleton
+@RolesAllowed({"AdminRole"})
 public class TestRestController {
 
 	@GET
 	@Produces("application/json")
-	public Response getTest() {
-		return Response.ok(new User("hari", "frebort")).build();
+	public User getTest() {
+		return new User("hari", "frebort");
 	}
 
 }
