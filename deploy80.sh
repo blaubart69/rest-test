faeaecd0f7971d3d7e0b8ebc2e442a626f5bf337
Bernhard Spindler <bernhard.spindler.75@gmail.com>
2024-12-11 13:58:59 +0100

    switch to Jakarta namespace to test out JBoss 8

--- deploy80.sh
+++ deploy80.sh
@@ -1,3 +1,4 @@
 #/bin/sh/
 mvn clean install 
 cp target/rest-test.war ~/dev/schaefer/jboss-eap-8.0/standalone/deployments/
+
--- src/main/java/at/spindi/TestRestController.java
+++ src/main/java/at/spindi/TestRestController.java
@@ -1,9 +1,9 @@
 package at.spindi;
 
-import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 
 @Path("/admins")
 @RolesAllowed({"AdminRole"})
